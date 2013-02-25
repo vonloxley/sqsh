50f8921ff545258c0308a2bb210dfad8ddf3b343
mwesdorp <mwesdorp>
2013-02-25 10:46:09 +0000

    sqsh-2.1.9 doc changes

--- doc/buildman.sh
+++ doc/buildman.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-pod2man --center sqsh-2.1.8 --name sqsh --release 2.1.8 --section 1 sqsh.pod sqsh.1
-
-#pod2man sqsh.pod > sqsh.1
-
+pod2man --center sqsh-2.1.9 --name sqsh --release 2.1.9 --section 1 sqsh.pod sqsh.1
 pod2html --noindex --title sqsh sqsh.pod > sqsh.html
