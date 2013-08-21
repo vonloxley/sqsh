902ca8ad08a7819830e388937f125210ad3666b0
mwesdorp <mwesdorp>
2013-08-21 14:07:26 +0000

    sqsh-2.4 New features and bugfixes

---
+++ src/sqsh_parser/tsql.h
@@ -0,0 +1,5 @@
+#define INSUP                           1
+#define TOKEN                           2
+#define FROM                            3
+#define COMMA                           4
+#define JOIN                            5
